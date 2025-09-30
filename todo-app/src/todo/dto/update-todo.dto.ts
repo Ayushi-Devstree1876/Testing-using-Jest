de4105787f5cb31cf47991480c32a5d4bbf637f0
AYUSHI PATEL <ayushi.p@devstree.in>
2025-09-30 11:39:14 +0530

    Delete todo-app directory

--- todo-app/src/todo/dto/update-todo.dto.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTodoDto } from './create-todo.dto';
-
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {}
